62e87ecf158a9099ab3b623dfe6b720a188faecb
Kyle Bloom <kylejbloom@gmail.com>
2022-09-16 22:48:31 +0100

    Add info trace

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use lambda_http::{run, service_fn, Body, Error, Request, Response};
 use svg::node::element::path::Data;
 use svg::node::element::Path;
 use svg::Document;
+use tracing::info;
 
 fn create_svg() -> Document {
     let data = Data::new()
@@ -20,23 +21,20 @@ fn create_svg() -> Document {
     Document::new().set("viewBox", (0, 0, 70, 70)).add(path)
 }
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
     let svg_doc = create_svg();
     let mut svg_data: Vec<u8> = Vec::new();
 
     svg::write(&mut svg_data, &svg_doc)?;
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
         .header("content-type", "image/svg+xml")
-        .body(Body::Binary(svg_data))
+        .body(svg_data.into())
         .map_err(Box::new)?;
+
+    info!("{resp:?}");
+
     Ok(resp)
 }
 
